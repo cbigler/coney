40e6c054b6498adf709157bc75961a4b61954ff6
cbigler <canute@cbigler.com>
2016-02-03 21:38:51 -0500

    Add docs

--- jackrabbit/constants.py
+++ jackrabbit/constants.py
@@ -5,3 +5,4 @@ version = (0, 0, 1)
 "Module version tuple"
 
 RABBIT_REPLYTO_QUEUE = 'amq.rabbitmq.reply-to'
+"Special RabbitMQ direct reply to queue. See https://www.rabbitmq.com/direct-reply-to.html"
